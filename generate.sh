b22a9e242d8e4a182dbf381edb0b1cce309ab0ca
Mike Friesen <mfriesen@gmail.com>
2025-11-02 20:02:14 -0600

    Updated github repository information

--- generate.sh
+++ generate.sh
@@ -3,5 +3,6 @@
 openapi-generator generate \
   -i https://raw.githubusercontent.com/formkiq/formkiq-core/refs/heads/master/docs/openapi/openapi-jwt.yaml \
   -g python \
+  --git-user-id formkiq --git-repo-id formkiq-client-sdk-python \
   -o . \
   --additional-properties=packageName=formkiq_client
